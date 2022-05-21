7a8a0ed9c97f91d82e017e29b53585dba8b2b35e
sakho13 <wecl.khotaro@gmail.com>
2022-05-22 01:54:35 +0900

    ホットリロード対応

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // configureWebpack: {
-  //   watch: true,
-  // },
+  configureWebpack: {
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    }
+  },
   devServer: {
     proxy: "http://localhost:8890"
   },
 
-  transpileDependencies: [
-    'quasar'
-  ],
+  transpileDependencies: true,
 
   pluginOptions: {
     quasar: {
