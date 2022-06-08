547488f212fec039f37fbcfc73defc44cd26e54c
sakho13 <wecl.khotaro@gmail.com>
2022-06-08 12:47:00 +0900

    誤字酒精

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,51 +1,60 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import { onAuthStateChanged, signOut } from "firebase/auth"
-import TopView from '../views/TopView.vue'
-import { FirebaseAuth } from '@/main'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+
+import { FirebaseAuth } from "@/main";
+import TopView from "../views/TopView.vue";
+import { userStore } from "../pinia/userStore";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Top',
+    path: "/",
+    name: "Top",
     component: TopView,
   },
   {
-    path: '/sign_in',
-    name: 'SignIn',
-    component: () => import('../views/SignInView.vue')
+    path: "/sign_in",
+    name: "SignIn",
+    component: () => import("../views/SigninView.vue"),
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue'),
+    path: "/home",
+    name: "Home",
+    component: () => import("../views/HomeView.vue"),
     meta: {
       requireAuth: true,
-    }
-  }
-]
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 router.beforeEach(async (to, from, next) => {
+  const userStoreObj = userStore();
+
+  if (to.name === "Home") {
+    if (userStoreObj.jwt !== "") {
+      return next();
+    } else {
+      return next({ name: "SignIn" });
+    }
+  }
+
   if (to.matched.some((rec) => rec.meta.requireAuth)) {
     // 認証チェック
     await onAuthStateChanged(FirebaseAuth, async (user) => {
       if (user !== null) {
-        if (to.name === "SignIn") {
-          //
-        }
-        next()
+        return next();
       } else {
-        await signOut(FirebaseAuth)
-        router.replace({ name: "SignIn" })
+        await signOut(FirebaseAuth);
+        return next({ name: "SignIn" });
       }
-    })
+    });
   } else {
-    next()
+    return next();
   }
-})
+});
 
-export default router
+export default router;
