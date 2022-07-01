2c659d1c7c76013af3114cf0b834c72bcaf2bd8b
sakho13 <wecl.khotaro@gmail.com>
2022-07-01 14:29:21 +0900

    メッセージ追加

--- backend/maps/error_msg_map.go
+++ backend/maps/error_msg_map.go
@@ -3,6 +3,8 @@ package maps
 var ErrorMsgMap map[uint]string = map[uint]string{
 	1000: "Tokenデコード処理エラー",
 	1001: "UUID生成エラー",
+	1002: "UUIDパースエラー",
+	1003: "FirebaseJWT未入力エラー",
 
 	2000: "ユーザ登録エラー",
 }
