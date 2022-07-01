0b2a24854f2e9eb92ddb6835c44f71527db54b03
sakho13 <wecl.khotaro@gmail.com>
2022-07-01 13:57:15 +0900

    モデル修正

--- backend/models/user.go
+++ backend/models/user.go
@@ -14,20 +14,8 @@ type User struct {
 
 	// NickName for show
 	NickName string `gorm:"size:64;not null"`
-	// SelfIntroduce is introducing self
-	SelfIntroduce string `gorm:"not null"`
-	// Sex 0: woman, 1: man, 99: otherwise, 100: unknown
-	Sex uint `gorm:"not null"`
-	// Age: if this is 0 then private
-	Age uint `gorm:"not null"`
 
 	IconImg string `gorm:"not null"`
 
-	// Permission 0: normal
-	Permission uint
-
-	// Status 0: normal, 1: stranger, 99: danger
-	Status uint `gorm:"not null"`
-
-	Initialized bool `gorm:"not null"`
+	TwitterID string `gorm:"not null"`
 }
