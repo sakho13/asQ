9f88650f77bb840a1f582e5b66f2d092175995c9
sakho13 <wecl.khotaro@gmail.com>
2022-07-03 18:51:50 +0900

    ファイル名変更

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,11 +8,11 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import Axios from "axios";
-import { FirebaseConfig } from "../firebase_config";
+import { firebaseConfig } from "../firebase_config";
 
 loadFonts();
 
-export const FirebaseApp = initializeApp(FirebaseConfig);
+export const FirebaseApp = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(FirebaseApp);
 
 Axios.defaults.baseURL = "http://localhost:8890";
